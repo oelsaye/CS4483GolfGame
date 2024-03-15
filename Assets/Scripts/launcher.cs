186f98a183895e40f92314bc18c98ccdbfb3702d
ceyx0001 <45667885+ceyx0001@users.noreply.github.com>
2024-03-14 21:50:03 -0400

    Added course 7 and 8

--- Assets/Scripts/launcher.cs
+++ Assets/Scripts/launcher.cs
@@ -2,15 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class launcher : MonoBehaviour
+public class launcherObstacle : MonoBehaviour
 {
     public float launchForce = 500; // Force applied to the ball
+    public string direction = "forward";
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("launched");
         Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
 
-        rb.AddForce(0, launchForce, launchForce / 2);
+        switch (direction) {
+            case "forward":
+                rb.AddForce(0, launchForce, launchForce / 2);
+                break;
+            case "backward":
+                rb.AddForce(0, launchForce, -launchForce / 2);
+                break;
+            case "right":
+            rb.AddForce(launchForce / 2, launchForce, 0);
+                break;
+            case "left":
+            rb.AddForce(-launchForce / 2, launchForce, 0);
+                break;
+        }
     }
 }
